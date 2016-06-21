3b0eab51a4bb30bd05b9c941e867c3066d4418a0
Jennifer Swartz <swartzjenn@gmail.com>
2016-06-20 21:32:33 -0400

    Set up add files and print functions

--- todolist.rb
+++ todolist.rb
@@ -1,7 +1,48 @@
 class TodoList
-    # methods and stuff go here
+  attr_accessor :title, :items
+  
+  #methods and stuff go here
+  def initialize(list_title)
+    @title = list_title
+    @items = Array.new
+  end
+  
+  def add_item(new_item)
+    item = Item.new(new_item)
+    @items.push(item)
+  end
+  
+  def delete_item(new_item)
+    @items.delete(item)
+  end
+  
+  def print_items
+    puts "-" * 20
+    puts @title
+    puts "-" * 20
+    @items.each_with_index do |item, index|
+      puts "#{index + 1} - #{@items[index].description}"  
+    end
+  end 
+#   def print_items
+#     print_list_title
+#     @items.each_with_index do |task, index|
+#       puts "#{index +1} - #{task.print_task}"   
+#     end
+#   end
 end
 
 class Item
-    # methods and stuff go here
-end
+  attr_reader :description, :completed_status
+  
+  #Initialize item with a description and marked as not complete
+  def initialize(item_description)
+    @description = item_description
+    @completed_status = false
+  end
+end 
+#  def print_items #(item_description)
+#    puts "test"  #item_description
+#    puts "Completed: " @complated_status
+#  end
+  
--- udacitask.rb
+++ udacitask.rb
@@ -1,14 +1,24 @@
 require_relative 'todolist.rb'
 
 # Creates a new todo list
+mylist = TodoList.new("Jennifer's To Do List")
 
 # Add four new items
+mylist.add_item("Go to Fresh Market")
+mylist.add_item("Buy steak")
+mylist.add_item("Start grill")
+mylist.add_item("Grill steak")
+
+
 
 # Print the list
+mylist.print_items
 
 # Delete the first item
+#mylist.delete_item(0)
 
 # Print the list
+#puts mylist.print_list
 
 # Delete the second item
 
