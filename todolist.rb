39e8cee40c0672a28c8e59cf732488511f3cbd3f
Jennifer Swartz <swartzjenn@gmail.com>
2016-06-22 14:31:47 -0400

    All required features added and working

--- todolist.rb
+++ todolist.rb
@@ -7,35 +7,44 @@ class TodoList
     @items = Array.new
   end
   
+  #Add new item to array
   def add_item(new_item)
     item = Item.new(new_item)
     @items.push(item)
   end
   
+  #Delete items from the array using the index value
   def delete_item(index)
     @items.delete_at(index)
   end
-  
-  def print_list_header
-    puts "-" * 20
-    puts @title
-    puts "-" * 20
-    puts "All My 'To Dos' ".ljust(25) + "Completed?"
+
+  #Update the status of an item (completed or not completed) using index and 
+  #true for completed and false for not completed  
+  def update_item_status(index, status)
+    @items[index].change_completed_status(status)
+  end
+
+  #Rename title of list
+  def rename_list_title(new_list_title)
+    @title = new_list_title
   end
   
+  #Print the header for the To Do List  
+  def print_list_header
+    puts "-" * 30
+    puts @title
+    puts "-" * 30
+    puts "TO DOs".ljust(45) + "Completed?"
+  end
+
+  #Print the tasks in the list in a numbered fashion 
   def print_items
     print_list_header
     @items.each_with_index do |task, index|
       puts "#{index + 1} - " + "#{task.print_task}"
     end
+    puts ""
   end
-
-
-  
-#  def print_list_with_items
-#    print_list
-#    print_each_item
-#  end
 end  
 
 class Item
@@ -46,9 +55,19 @@ class Item
     @description = item_description
     @completed_status = false
   end
+
+  #changes status of list  
+  def change_completed_status(status)
+    @completed_status = status
+  end
+
+  #will return false if not completed and true if completed  
+  def completed?
+    @completed_status
+  end
  
   def print_task
-    "#{@description.ljust(20)} {@completed_status}?"
+    "#{@description.ljust(42)} #{completed? ? "[X]" : "[ ]"}" 
   end 
 end  
 
--- udacitask.rb
+++ udacitask.rb
@@ -1,33 +1,38 @@
 require_relative 'todolist.rb'
 
 # Creates a new todo list
-mylist = TodoList.new("Jennifer's To Do List")
+mylist = TodoList.new("Jenn's Sports-Fandom To Do List")
 
 # Add four new items
-mylist.add_item("Go to Fresh Market")
-mylist.add_item("Buy steak")
-mylist.add_item("Start grill")
-mylist.add_item("Grill steak")
+mylist.add_item("Watch Reds game")
+mylist.add_item("Watch Reds lose again")
+mylist.add_item("Complain that Reds lost again")
+mylist.add_item("Count days until Buckeyes start season")
 
 # Print the list
 mylist.print_items
 
 # Delete the first item
-#mylist.delete_item(0)
+mylist.delete_item(0)
 
 # Print the list
-#mylist.print_items
+mylist.print_items
 
 # Delete the second item
-#mylist.delete_item(1)
+mylist.delete_item(1)
 
 # Print the list
-#mylist.print_items
+mylist.print_items
 
 # Update the completion status of the first item to complete
+mylist.update_item_status(0, true)
+
 
 # Print the list
+mylist.print_items
 
 # Update the title of the list
+mylist.rename_list_title("Reds & Buckeye Fan To Do List")
 
 # Print the list
+mylist.print_items
